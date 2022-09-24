7e0b99a179225478022f4b4fedcf61a023fd0ebf
Sergey Misyuk <smisyuk@gmail.com>
2022-09-24 22:45:48 +0300

    task 6 done

---
+++ js/task-06.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Напиши скрипт, який під час втрати фокусу на інпуті(подія blur),
+// перевіряє його вміст щодо правильної кількості введених символів.
+
+
+// Яка кількість символів повинна бути в інпуті,
+// зазначається в його атрибуті data - length.
+// Якщо введена правильна кількість символів,
+// то border інпуту стає зеленим, якщо неправильна кількість
+// - червоним.
+// Для додавання стилів використовуй CSS - класи valid і invalid,
+// які ми вже додали у вихідні файли завдання.
+
+const refs = {
+    input: document.querySelector('#validation-input'),
+    dataLength: document.querySelector('#validation-input').getAttribute('data-length'),
+}
+
+const checkInput = (event) => {
+    if (Number(refs.dataLength) === refs.input.value.length) {
+        refs.input.classList.add('valid')
+    } 
+
+    if (Number(refs.dataLength) !== refs.input.value.length) {
+        refs.input.classList.add('invalid')
+    }
+
+    if (Number(refs.dataLength) === refs.input.value.length
+        && refs.input.classList.contains('invalid')) {
+        refs.input.classList.replace('invalid', 'valid')
+    }
+    
+    if (Number(refs.dataLength) !== refs.input.value.length
+        && refs.input.classList.contains('valid')) {
+        refs.input.classList.replace('valid', 'invalid')
+    }        
+}
+
+refs.input.addEventListener('blur', checkInput)
